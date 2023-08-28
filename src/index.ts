493d248783cf165f0d255557ad037c06c17061b5
Josias Klaus <josias.klaus@web.de>
2023-08-28 03:16:05 +0200

    Implemented router to add /api prefix 🌐

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express';
+import express, { Express, Router } from 'express';
 import dotenv from 'dotenv';
 import school from './routes/school';
 import form from './routes/form';
@@ -7,14 +7,16 @@ import cors from 'cors';
 dotenv.config();
 
 const app: Express = express();
+const router: Router = express.Router();
 app.use(express.json());
 app.use(cors());
 
 export let database: Db;
 new MongoClient(process.env.MONGODB_URL || "mongodb://localhost:27017/loewenstark").connect().then((client) => database = client.db()).catch((err) => { throw err; });
 
-app.use('/school', school);
-app.use('/submit', form);
+router.use('/school', school);
+router.use('/submit', form);
+app.use('/api', router);
 
 const server = app.listen(process.env.PORT || 8080, () => {
   console.log(`Mampfaxo » Server is running at http://localhost:${process.env.PORT || 8080}`);
